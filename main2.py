11fad73558e71884cf7456b1226db62820de6242
DanielDuarteRessolve <daniel.duarte@ressolve.ai>
2023-12-19 15:47:00 -0500

    check_moments

--- main2.py
+++ main2.py
@@ -55,60 +55,80 @@ def map_moments(moments, categories_found):
         result.append(elem)
     return result
 
-class bidirectional_iterator(object):
-    def __init__(self, collection):
-        self.collection = collection
-        self.index = 0
-
-    def next(self):
-        try:
-            result = self.collection[self.index]
-            self.index += 1
-        except IndexError:
-            raise StopIteration
-        return result
-
-    def prev(self):
-        self.index -= 1
-        if self.index < 0:
-            raise StopIteration
-        return self.collection[self.index]
-
-    def __iter__(self):
-        return self
-    
 def check_categories(moment_result, categories_found):
     value_mode=3
 
     for current_moment in moment_result:
         current_moment_categories = current_moment["categories_found"]
         valid_ids = []
-        for id in current_moment["categories_found_id"]:
-            foward = categories_found[id:id+value_mode]
-            backward = categories_found[id-(value_mode+1):id-1]
-            filtered = []
-            
-            for category in current_moment_categories:
-                if (category["id"] in [i["id"] for i in foward] or category["id"] in [i["id"] for i in backward]):
-                    filtered.append(category)
-                    valid_ids.append(category["id"])
+        filtered = []
+        if len(current_moment["categories_found_id"]) >= 2:
+            for id in current_moment["categories_found_id"]:
+                foward = categories_found[id:id+value_mode]
+                backward = categories_found[id-(value_mode+1):id-1]
 
-            print(current_moment["moment"], "--", id, "--", categories_found[id-1]["category"], "-", categories_found[id-1]["subcategory"])
-            print("current: ", current_moment_categories)
-            print("foward: ", foward)
-            print("backware: ", backward)
-            print(filtered)
+                
+                for category in current_moment_categories:
+                    if (category["id"] in [i["id"] for i in foward] or category["id"] in [i["id"] for i in backward]):
+                        # if category["id"]-current_moment_categories[index-1]["id"]<=value_mode:
+                        filtered.append(category)
+                        valid_ids.append(category["id"])
+        else:
+            filtered = current_moment_categories
+            valid_ids = [i["id"] for i in filtered]
+        valid_ids.sort()
         current_moment["categories_found"] = filtered
         current_moment["categories_found_id"] = valid_ids
 
-        # ids = moment['categories_found_id']
-        # for i in range(len(ids)):
-        #     if i < len(ids)-1:
-        #         if ids[i+1]-ids[i]>=value_mode:
-        #             ids = ids[:i+1]
-
-        # moment['categories_found_id'] = ids
     return moment_result
+
+def check_moments(moment_result, moments, categories_found):
+    value_mode=3
+    for i, current_moment in enumerate(moment_result):
+ 
+    # categorias de los momentos del archivo momentos original
+        current_result_moment_order = current_moment['order']
+        current_original_moments = next((element for element in moments if element["order"] == current_result_moment_order), None)
+        original_previous_moment = moments[i - 1] if moments[i]['order'] != 1 else None
+        original_next_moment = moments[i + 1] if i < len(moments) - 1 else None
+        
+        if original_previous_moment != None or original_next_moment != None:
+            original_previous_moment = moments[i]
+
+        if len(current_moment["categories_found_id"]) >= 2:
+            for current_id in current_moment["categories_found_id"]:
+                foward = categories_found[current_id:current_id+value_mode]
+                backward = categories_found[current_id-(value_mode+1):current_id-1]
+                is_back = False
+                is_foward = False
+                print("curren_category_id: ", current_id, "\n")
+                print("fowared:\n ", foward)
+                print("original_next_moment: ", original_next_moment)
+                print("\nbackward:\n ", backward)
+                print("original_previous_moment: ", original_previous_moment)
+                
+                for category in backward:
+                    _category = f"{category['category']}-{category['subcategory']}" 
+                    if (_category in current_moment["categories_found"] or _category in original_previous_moment["categories"]):
+                        is_back = True
+                        break
+                for category in foward:
+                    _category = f"{category['category']}-{category['subcategory']}" 
+                    if (_category in current_moment["categories_found"] or _category in original_next_moment["categories"]):
+                        is_foward = True
+                        break
+                if not is_back and not is_foward:
+                    current_moment["categories_found_id"].remove(current_id)
+                    # current_moment["categories_found"].remove(current_id)
+
+        else:
+            continue
+
+        return moment_result
+            # print(f"MOMENTO RESULTADO ACTUAL {current_original_moments}")
+            # print(f"las categorias colindantes en el orginal por izq:{original_previous_moment['categories']}")
+            # print(f"las categorias colindantes en el orginal por der:{original_next_moment['categories']}","\n" + "=" * 30 + "\n")
+            # verificacion de orden por categorias comparando el resultado con el orden de moments originallt:
 
 
 result = map_moments(moments, categories_found)
@@ -116,6 +136,9 @@ with open('resources/result_map.json', 'w') as json_file:
     json.dump(result, json_file, indent=2)
 result = check_categories(result, categories_found)
 with open('resources/result_cat.json', 'w') as json_file:
+    json.dump(result, json_file, indent=2)
+result = check_moments(result, moments, categories_found)
+with open('resources/result_moments.json', 'w') as json_file:
     json.dump(result, json_file, indent=2)
  
 # marks = [65, 71, 68, 74, 61]
